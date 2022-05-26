82c29a979ca9501cd52be76ca7b7251c0585fc4a
Junior Vieira <junior.vieira.1990@gmail.com>
2022-05-26 12:25:25 -0300

    User insert

--- src/main/java/com/learningspring/intro/controller/UserController.java
+++ src/main/java/com/learningspring/intro/controller/UserController.java
@@ -4,14 +4,16 @@ import com.learningspring.intro.model.entities.Order;
 import com.learningspring.intro.model.entities.User;
 import com.learningspring.intro.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -23,12 +25,26 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
         List<User> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
+        return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable("id") Long id) {
-        User user = userService.findById(id);
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<Object> findById(@PathVariable("id") Long id) {
+        Optional<User> user = userService.findById(id);
+        if (!user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+            return ResponseEntity.status(HttpStatus.OK).body(user.get());
+
     }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).body(savedUser);
+    }
+
+
 }
--- src/main/java/com/learningspring/intro/services/UserService.java
+++ src/main/java/com/learningspring/intro/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,7 +18,10 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User findById(Long id) {
-        return userRepository.findById(id).orElse(null);
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+    public User save(User user) {
+        return userRepository.save(user);
     }
 }
